6005983dfbc3487fae5d5b9ed103bba12cfe1cca
patrickm10 <pmejia@wpi.edu>
2021-04-15 16:38:07 -0400

    Made function stubs for Student and Course

--- src/main/java/Course.java
+++ src/main/java/Course.java
@@ -1,2 +1,44 @@
+import java.util.List;
+
 public class Course {
+    private String name;
+    private List<Student> students;
+    Course course = new Course();
+
+    public Course(){
+        this.name = getName();
+        this.students = getStudents();
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public void setName(String name){
+        course.setName(name);
+    }
+
+    public List<Student> getStudents(){
+        return this.students;
+    }
+
+    public List<Student> setStudents(List<Student> students){
+       return course.setStudents(students);
+    }
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "courseName='" + name + '\'' +
+                ", students=" + students +
+                '}';
+    }
+
+    public void listCourseStudents(){
+        System.out.println(course.students);
+    }
+
+    public void addStudent(Student student){
+        students.add(student);
+    }
 }
--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -1,2 +1,51 @@
+import java.util.List;
+
 public class Student {
+    private String name;
+    private List<Course> courses;
+    private List<Student> students;
+    Student student = new Student();
+
+    public Student(){
+        this.name = getName();
+        this.courses= getCourses();
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public static void setName(String name){
+       try {
+           Student.setName(name);
+       }
+       catch(Exception e){
+           e.printStackTrace();
+        }
+    }
+
+    public List<Course> getCourses(){
+        return this.courses;
+    }
+
+    public static List<Course> setCourses(List<Course> courses){
+        return Student.setCourses(courses);
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "studentName='" + name + '\'' +
+                ", students=" + students +
+                '}';
+    }
+
+    public void listStudentCourses(){
+        System.out.println(student.courses);
+    }
+
+    public void addCourse(Course course){
+        courses.add(course);
+    }
+
 }
