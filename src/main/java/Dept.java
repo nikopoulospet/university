ffb26f06fee5dfb1545d3b05113e9d7a356c1a6b
nikopoulospet <xxpeteynikxx@gmail.com>
2021-04-16 10:08:30 -0400

    created methods for college and dept

--- src/main/java/Dept.java
+++ src/main/java/Dept.java
@@ -1,2 +1,36 @@
+import java.util.List;
+
 public class Dept {
+    private String deptName;
+    private List<Course> courses;
+
+
+    public String getDeptName() {
+        return deptName;
+    }
+
+    public void setDeptName(String deptName) {
+        this.deptName = deptName;
+    }
+
+    public List<Course> getCourses() {
+        return courses;
+    }
+
+    public void setCourses(List<Course> courses) {
+        this.courses = courses;
+    }
+
+    public int countCourses(){
+        return courses.size();
+    }
+
+    public int countStudentsInDeptCourses() {
+        int students = 0;
+        for(int i=0;i<courses.size();i++){
+            students += courses.get(i).getStudents().size();
+        }
+
+        return students;
+    }
 }
